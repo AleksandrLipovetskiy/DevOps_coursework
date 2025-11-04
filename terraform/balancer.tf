5a801a1f475dac931f090e7150f4ca1bb12a143b
AleksandrLipovetskiy <stsledov@gmail.com>
2025-11-04 20:42:49 +0300

    попытка 10 поднять инфру

--- terraform/balancer.tf
+++ terraform/balancer.tf
@@ -5,22 +5,42 @@ resource "yandex_lb_target_group" "k8s_web_targets" {
 
   target {
     subnet_id = yandex_vpc_subnet.k8s_subnet[0].id
-    address   = yandex_compute_instance.worker[0].network_interface.0.ip_address
+    address   = yandex_compute_instance.worker[0].network_interface[0].ip_address  # приватный IP
   }
   target {
     subnet_id = yandex_vpc_subnet.k8s_subnet[1].id
-    address   = yandex_compute_instance.worker[1].network_interface.0.ip_address
+    address   = yandex_compute_instance.worker[1].network_interface[0].ip_address
   }
   target {
     subnet_id = yandex_vpc_subnet.k8s_subnet[2].id
-    address   = yandex_compute_instance.worker[2].network_interface.0.ip_address
+    address   = yandex_compute_instance.worker[2].network_interface[0].ip_address
   }
 }
 
-# Создание сетевого балансировщика нагрузки для Kubernetes API (мастер-ноды)
+# Сетевая группа целей для балансиравщика API (мастер-ноды)
+resource "yandex_lb_target_group" "k8s_api_targets" {
+  name      = "k8s-api-targets"
+  region_id = "ru-central1"
+
+  target {
+    subnet_id = yandex_vpc_subnet.k8s_subnet[0].id
+    address   = yandex_compute_instance.master[0].network_interface[0].ip_address  # приватный IP
+  }
+  target {
+    subnet_id = yandex_vpc_subnet.k8s_subnet[1].id
+    address   = yandex_compute_instance.master[1].network_interface[0].ip_address
+  }
+  target {
+    subnet_id = yandex_vpc_subnet.k8s_subnet[2].id
+    address   = yandex_compute_instance.master[2].network_interface[0].ip_address
+  }
+}
+
+# Сетевой балансировщик нагрузки для API Kubernetes
 resource "yandex_lb_network_load_balancer" "k8s_api_lb" {
   name = "k8s-api-lb"
   type = "external"
+
   listener {
     name        = "api-listener"
     port        = 6443
@@ -43,30 +63,11 @@ resource "yandex_lb_network_load_balancer" "k8s_api_lb" {
   }
 }
 
-# Создание группы целей для API балансировщика (мастер-ноды)
-resource "yandex_lb_target_group" "k8s_api_targets" {
-  name      = "k8s-api-targets"
-  region_id = "ru-central1"
-
-  target {
-    subnet_id = yandex_vpc_subnet.k8s_subnet[0].id
-    address   = yandex_compute_instance.master[0].network_interface.0.ip_address
-  }
-  target {
-    subnet_id = yandex_vpc_subnet.k8s_subnet[1].id
-    address   = yandex_compute_instance.master[1].network_interface.0.ip_address
-  }
-  target {
-    subnet_id = yandex_vpc_subnet.k8s_subnet[2].id
-    address   = yandex_compute_instance.master[2].network_interface.0.ip_address
-  }
-}
-
-# Создание Application Load Balancer для веб-приложения
+# Application Load Balancer с автоматическим выделением одного публичного IP
 resource "yandex_alb_load_balancer" "k8s_web_lb" {
-  name        = "k8s-web-lb"
-  network_id  = yandex_vpc_network.k8s_network.id
-  region_id   = "ru-central1"
+  name       = "k8s-web-lb"
+  network_id = yandex_vpc_network.k8s_network.id
+  region_id  = "ru-central1"
 
   allocation_policy {
     location {
@@ -87,9 +88,7 @@ resource "yandex_alb_load_balancer" "k8s_web_lb" {
     name = "web-listener"
     endpoint {
       address {
-        external_ipv4_address {
-          address = "auto"
-        }
+        external_ipv4_address {}  # Публичный IP выделяется автоматически без указания ключа address
       }
       ports = [80, 443]
     }
@@ -106,8 +105,8 @@ resource "yandex_alb_http_router" "k8s_web_router" {
 }
 
 resource "yandex_alb_virtual_host" "k8s_web_host" {
-  name            = "k8s-web-host"
-  http_router_id  = yandex_alb_http_router.k8s_web_router.id
+  name           = "k8s-web-host"
+  http_router_id = yandex_alb_http_router.k8s_web_router.id
 
   route {
     name = "default-route"
@@ -128,7 +127,7 @@ resource "yandex_alb_backend_group" "k8s_web_backend_group" {
   name = "k8s-web-backend-group"
   http_backend {
     name             = "web-backend"
-    target_group_ids = [yandex_lb_target_group.k8s_web_targets.id]
+    target_group_ids  = [yandex_lb_target_group.k8s_web_targets.id]
     port             = 80
     healthcheck {
       timeout  = "5s"
