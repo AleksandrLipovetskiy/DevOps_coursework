06e0e16ab196a3a9a78a7a69922667643178aa06
AleksandrLipovetskiy <stsledov@gmail.com>
2026-04-26 17:32:14 +0300

    правки9

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -2,15 +2,3 @@ data "yandex_iam_service_account" "terraeditor" {
   name      = "terraeditor"
   folder_id = var.folder_id
 }
-
-resource "yandex_resourcemanager_folder_iam_member" "terraeditor_editor" {
-  folder_id = var.folder_id
-  role      = "editor"
-  member    = "serviceAccount:${data.yandex_iam_service_account.terraeditor.id}"
-}
-
-resource "yandex_resourcemanager_folder_iam_member" "terraeditor_registry_reader" {
-  folder_id = var.folder_id
-  role      = "cr.reader"
-  member    = "serviceAccount:${data.yandex_iam_service_account.terraeditor.id}"
-}
