b03104ecc2d633127230090ec19f7a72c11e9aa3
AleksandrLipovetskiy <stsledov@gmail.com>
2026-05-20 12:19:34 +0300

    Исправил регистри

--- terraform/registry.tf
+++ terraform/registry.tf
@@ -1,14 +1,9 @@
 resource "yandex_container_registry" "registry" {
   name      = var.registry_name
-  folder_id = var.folder_id  # если нужно явно указать
+  folder_id = var.folder_id
   
   labels = {
     purpose = "app-nspc"
     env     = "production"
   }
-}
-
-resource "yandex_container_repository" "app_nspc" {
-  registry_id = yandex_container_registry.registry.id
-  name        = var.registry_repo_name
 }
